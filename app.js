0f9355ed1b97833e0a4e02eccd7e62b30f6a3a7f
Matt Pahuta <mhpahuta@gmail.com>
2021-08-05 15:41:55 -0700

    Mole statuses, interval functions added

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-// Global variables
-let runAgainAt = Date.now() + 100;
 
-// Function to handle mole sadness interval
-function getSadInterval() {
-  return Date.now() + 1000;
-}
-
-function nextFrame() {
-  const now = Date.now();
-
-  if (runAgainAt <= now) {
-    for (let i = 0; i < moles.length; i++) {
-
-    }
-    runAgainAt = now + 100;
-  }
-
-  requestAnimationFrame(nextFrame);
-}
-
+// moles array
 const moles = [
   {
     status: 'sad', // sad, hungry, leaving, gone
@@ -82,3 +63,65 @@ const moles = [
   },
 
 ];
+
+// Function to handle mole sadness interval
+function getSadInterval() {
+  return Date.now() + 1000;
+}
+
+function getGoneInterval() {
+  return Date.now() + Math.floor(Math.random() * 18000) + 2000;
+}
+
+function getHungryInterval() {
+  return Date.now() + Math.floor(Math.random() * 3000) + 2000;
+}
+
+// function to switch mole status
+function getNextStatus(mole) {
+  switch (mole.status) {
+    case "sad":
+      mole.next = getSadInterval();
+      mole.status = "leaving";
+      mole.node.children[0].src = "./imgs/mole-leaving.png";
+      break;
+    case "leaving":
+      mole.next = getGoneInterval();
+      mole.status = "gone";
+      mole.node.children[0].classList.add("gone");
+      break;
+    case "gone":
+      mole.next = getHungryInterval();
+      mole.status = "hungry";
+      mole.node.children[0].classList.add("hungry");
+      mole.node.children[0].classList.remove("gone");
+      mole.node.children[0].src = "./imgs/mole-hungry.png";
+      break;
+    case "hungry":
+      mole.next = getSadInterval();
+      mole.status = "sad";
+      mole.node.children[0].classList.remove("hungry");
+      mole.node.children[0].src = "./imgs/mole-sad.png";
+      break;
+  }
+}
+
+// Game run function 
+
+let runAgainAt = Date.now() + 100;
+function nextFrame() {
+  const now = Date.now();
+
+  if (runAgainAt <= now) {
+    for (let i = 0; i < moles.length; i++) {
+      if (moles[i].next <= now) {
+        getNextStatus(moles[i]);
+      }
+    }
+    runAgainAt = now + 100;
+  }
+
+  requestAnimationFrame(nextFrame);
+}
+
+nextFrame();
