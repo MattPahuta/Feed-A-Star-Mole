ec6bc0cce32a0c4560846d7e77679f0774ad8f46
Matt Pahuta <mhpahuta@gmail.com>
2021-08-06 11:08:02 -0700

    Refactor JS and styles

--- app.js
+++ app.js
@@ -1,180 +1,166 @@
-
+const MIN_INTERVAL = 2000;
+const MAX_INTERVAL = 20000;
+const SAD_INTERVAL = 500;
+const HUNGRY_INTERVAL = 2000;
+const wormContainer = document.querySelector('.worm-container');
 let score = 0;
+
+// interval and status functions
+const getInterval = () => Date.now() + MIN_INTERVAL + Math.floor(Math.random() * MAX_INTERVAL);
+const getSadInterval = () => Date.now() + SAD_INTERVAL;
+const getKingStatus = () => Math.random() > 0.9;
+const getHungryInterval = () => Date.now() + HUNGRY_INTERVAL;
 
 // moles array
 const moles = [
   {
     status: 'sad', // sad, hungry, leaving, gone
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-0')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-1')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-2')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-3')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-4')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-5')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-6')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-7')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-8')
   },
   {
     status: 'sad',
     next: getSadInterval(),
-    king: false,
+    king: true,
     node: document.querySelector('#hole-9')
   },
-
 ];
 
-// Function to handle mole sadness interval
-function getSadInterval() {
-  return Date.now() + 1000;
-}
-
-function getGoneInterval() {
-  return Date.now() + Math.floor(Math.random() * 18000) + 2000;
-}
-
-function getHungryInterval() {
-  return Date.now() + Math.floor(Math.random() * 3000) + 2000;
-}
-
-function getKingStatus() {
-  return Math.random() > .9;
-}
-
 // function to switch mole status
-function getNextStatus(mole) {
+const getNextStatus = mole => {
   switch (mole.status) {
     case "sad":
     case "fed":
       mole.next = getSadInterval();
-      mole.status = "leaving";
       if (mole.king) {
         mole.node.children[0].src = "./imgs/king-mole-leaving.png";
       } else {
         mole.node.children[0].src = "./imgs/mole-leaving.png";
       }
+      mole.status = "leaving";
       break;
     case "leaving":
-      mole.next = getGoneInterval();
+      mole.next = getInterval();
+      mole.king = false;
+      mole.node.children[0].classList.toggle("gone", true);
       mole.status = "gone";
-      mole.node.children[0].classList.add("gone");
-      break;
-    case "gone":
-      mole.next = getHungryInterval();
-      mole.status = "hungry";
-      mole.king = getKingStatus();
-      mole.node.children[0].classList.add("hungry");
-      mole.node.children[0].classList.remove("gone");
-      if (mole.king) {
-        mole.node.children[0].src = "./imgs/king-mole-hungry.png";
-      } else {
-        mole.node.children[0].src = "./imgs/mole-hungry.png";
-      }
       break;
     case "hungry":
       mole.next = getSadInterval();
-      mole.status = "sad";
-      mole.node.children[0].classList.remove("hungry");
+      mole.node.children[0].classList.toggle("hungry", false);
       if (mole.king) {
         mole.node.children[0].src = "./imgs/king-mole-sad.png";
       } else {
         mole.node.children[0].src = "./imgs/mole-sad.png";
       }
+      mole.status = "sad";
+      break;
+    case "gone":
+      mole.next = getHungryInterval();
+      mole.king = getKingStatus();
+      mole.node.children[0].classList.toggle("hungry", true);
+      mole.node.children[0].classList.toggle("gone", false);
+      if (mole.king) {
+        mole.node.children[0].src = "./imgs/king-mole-hungry.png";
+      } else {
+        mole.node.children[0].src = "./imgs/mole-hungry.png";
+      }
+      mole.status = "hungry";
       break;
   }
-}
+};
 
-function feed (event) {
+const feed = event => {
   // check if target is an img or not hungry
   if (event.target.tagName !== 'IMG' || !event.target.classList.contains('hungry')) {
     return;
   }
   // feed mole
-  const mole = moles[parseInt(event.target.dataset.index)];
+  const mole = moles[+event.target.dataset.index];
   mole.status = 'fed';
   mole.next = getSadInterval();
+  mole.node.children[0].classList.toggle('hungry', false);
   if (mole.king) {
-    score += 2;
     mole.node.children[0].src = './imgs/king-mole-fed.png';
+    score += 20;
   } else {
-    score += 1;
     mole.node.children[0].src = './imgs/mole-fed.png';
+    score += 10;
   }
-  mole.node.children[0].classList.remove('hungry');
 
-  if (score >= 10) {
+  if (score >= 100) {
     win();
+    return;
   }
   // adjust the worm score meter
-  document.querySelector('.worm-container').style.width = `${10 * score}%`;
+  wormContainer.style.width = `${score}%`;
+};
 
-}
+// display win screen
+const win = () => {
+  document.querySelector(".bg").classList.toggle("hide", true);
+  document.querySelector(".win").classList.toggle("show", true);
+};
 
-function win() {
-  document.querySelector('.bg').classList.add('hide');
-  document.querySelector('.win').classList.remove('hide');
-}
-
-// Game run function 
-let runAgainAt = Date.now() + 100;
-function nextFrame() {
-  const now = Date.now();
-
-  if (runAgainAt <= now) {
-    for (let i = 0; i < moles.length; i++) {
-      if (moles[i].next <= now) {
-        getNextStatus(moles[i]);
-      }
-    }
-    runAgainAt = now + 100;
-  }
-
-  requestAnimationFrame(nextFrame);
-}
-
-// event listeners
 document.querySelector('.bg').addEventListener('click', feed);
 
-nextFrame();
+const nextFrame = () => {
+  const now = Date.now();
+  for (let i = 0; i < moles.length; i++) {
+    if (moles[i].next < now) {
+      getNextStatus(moles[i]);
+    }
+  }
+  requestAnimationFrame(nextFrame);
+};
+
+requestAnimationFrame(nextFrame);
